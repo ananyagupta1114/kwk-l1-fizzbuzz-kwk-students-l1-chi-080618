4505ddf6dfa3eb06d5c1b9255ec2cf8441b4bf4f
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-07 18:36:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,15 @@
+
+def fizzbuzz
 puts "What number do you select?"
 number = gets.chomp
 
 if number%3 == 0
   puts "Fizz"
-elseif number%5 == 0
+elsif number%5 == 0
   puts "Buzz"
-elseif number%5 && number%3 == 0
+elsif number%5 && number%3 == 0
   puts "FizzBuzz"
+else
+  puts number
   
+end
